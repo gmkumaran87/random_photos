53443045156b7b76dcfe7a15674dd72d77dbd018
Muthukumaran <gmkumaran87@gmail.com>
2021-09-29 13:56:35 +0530

    Pagination page increased from 10 to 15

--- photoFetch.js
+++ photoFetch.js
@@ -5,7 +5,7 @@ class FetchPhoto {
 
     fetchPhotos = async() => {
         const data = await fetch(
-            `https://api.unsplash.com/photos/?client_id=${this.ACCESS_KEY}`
+            `https://api.unsplash.com/photos/?client_id=${this.ACCESS_KEY}&page=1&per_page=15`
         );
 
         const photos = await data.json();
@@ -15,12 +15,10 @@ class FetchPhoto {
 
     findPhotos = async(query) => {
         const data = await fetch(
-            `https://api.unsplash.com/search/photos?client_id=${this.ACCESS_KEY}&page=1&query=${query}&per_page=30`
+            `https://api.unsplash.com/search/photos?client_id=${this.ACCESS_KEY}&page=1&query=${query}&per_page=15`
         );
         const photos = await data.json();
 
         return photos.results;
     };
 }
-
-//`<https://api.unsplash.com/search/photos?page=1&query=office>; rel="first", <https://api.unsplash.com/search/photos?page=1&query=office>; rel="prev", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="last", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="next"`;
