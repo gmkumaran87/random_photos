6c6fa95c4cf6b5b23f6aac661a0913501cd415b4
Muthukumaran <gmkumaran87@gmail.com>
2021-09-29 13:59:03 +0530

    Deleted the Input box after submit

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ init();
 const handleSubmit = async(e) => {
     e.preventDefault();
 
-    const inpValue = document.getElementById("photoImg").value;
+    const inpValue = document.getElementById("photoImg");
 
-    const newPhotos = await photosFetch.findPhotos(inpValue);
-    console.log(newPhotos);
+    const newPhotos = await photosFetch.findPhotos(inpValue.value);
+
     ui.displayImages(newPhotos);
+    inpValue.value = "";
 };
 const submitBtn = document.querySelector(".submit-btn");
 
